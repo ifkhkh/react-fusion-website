e10e6f518e06234a63de41cdd653e9e09f71fdae
风行 <7684823+clinkz_m@user.noreply.gitee.com>
2021-01-27 22:09:41 +0800

    misc: 自动格式化；Route 加 key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import routeMap from './page/routeConfig';
+import React from 'react'
+import './App.css'
+import routeMap from './page/routeConfig'
 
-import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
 
 function App() {
     const routes = routeMap.map((it, k) => {
-        it.key = k;
+        it.key = k
 
-        return <Route {...it} />;
-    });
+        return <Route {...it} key={k} />
+    })
 
     return (
         <div className="App">
@@ -20,7 +20,7 @@ function App() {
                 </Switch>
             </Router>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
