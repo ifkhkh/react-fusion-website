5b2fba66b4fd5d7d95cc54d00f9120aa058461f6
mollylee123 <447724702@qq.com>
2021-08-05 22:38:56 +0800

    feat：(Page) List 页面虚拟接口

--- src/page/List/index.js
+++ src/page/List/index.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
-// import { Button } from '@alifd/next'
+import React, { useState, useEffect } from 'react'
+import { Table } from '@alifd/next'
+import Request from '../../api/request'
+
+const log = console.log.bind(console)
 
 const List = function () {
-    // const [text, setText] = useState('List code 页面, 点我!')
+    const [data, setData] = useState([])
 
-    // const handleChange = () => setText('我变化了啦' + Date.now())
+    useEffect(() => {
+        Request.getList().then((res) => {
+            log(res)
+            if (res.success) {
+                setData(res.data)
+                log('data', data)
+            }
+        })
+    }, [])
 
     return (
-        <div>list</div>
+        <div>
+            <Table.StickyLock dataSource={data}>
+                <Table.Column title="Id" dataIndex="id" />
+                <Table.Column title="Name" dataIndex="name" />
+                <Table.Column title="Hair" dataIndex="hair.color" />
+                <Table.Column title="Pigu" dataIndex="pigu" />
+            </Table.StickyLock>
+        </div>
     )
 }
 
