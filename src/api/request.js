05e06a8a32c33687946436b37dbe64c7719f3e68
Ken Chan <ken@192.168.1.103>
2021-08-05 22:09:33 +0800

    feat: test api

---
+++ src/api/request.js
@@ -0,0 +1,48 @@
+class Request {
+    static getList() {
+        return new Promise((resolve) => {
+            const resp = {
+                success: true,
+                data: [
+                    {
+                        id: 1,
+                        name: 'dmdm1',
+                        hair: {
+                            color: 'gray1',
+                        },
+                        pigu: 'big1',
+                    },
+                    {
+                        id: 2,
+                        name: 'dmdm2',
+                        hair: {
+                            color: 'gray2',
+                        },
+                        pigu: 'big2',
+                    },
+                    {
+                        id: 3,
+                        name: 'dmdm3',
+                        hair: {
+                            color: 'gray3',
+                        },
+                        pigu: 'big3',
+                    },
+                    {
+                        id: 4,
+                        name: 'dmdm4',
+                        hair: {
+                            color: 'gray4',
+                        },
+                        pigu: 'big4',
+                    },
+                ],
+            }
+            const t = setTimeout(() => {
+                resolve(resp)
+            }, 300)
+        })
+    }
+}
+
+export default Request
