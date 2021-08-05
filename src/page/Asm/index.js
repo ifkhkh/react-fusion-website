2daccfe2db6060a7e2b88c8d23722a0ce9645ee5
mollylee123 <447724702@qq.com>
2021-08-05 21:55:50 +0800

    fix: test

--- src/page/Asm/index.js
+++ src/page/Asm/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Button } from '@alifd/next'
 
 const Asm = function () {
-    const [text, setText] = useState('asm code 页面, 点我')
+    const [text, setText] = useState('asm code 页面, 点我!')
 
     const handleChange = () => setText('我变化了啦' + Date.now())
 
