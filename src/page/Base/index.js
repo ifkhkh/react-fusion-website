e2ce9ea45147a88b112e2bfaa0376898679c975d
mollylee123 <447724702@qq.com>
2021-08-07 13:07:57 +0800

    feat:(comp)页面导航栏修改为更具配置数据自动渲染

--- src/page/Base/index.js
+++ src/page/Base/index.js
@@ -46,7 +46,46 @@ const menuList = [
         ],
     },
 ]
-
+const headerList = [
+    {
+        path: '/',
+        label: '主页',
+        key: 'home',
+    },
+    {
+        // path: '/',
+        label: '子菜单',
+        key: 'menu1',
+        sub: [
+            {
+                path: '/asm_code',
+                label: 'asm',
+                key: 'asm',
+            },
+            {
+                path: '/list',
+                label: 'List',
+                key: 'list',
+            },
+            {
+                path: '/list2',
+                label: 'List2',
+                key: 'list2',
+            },
+        ],
+    },
+    {
+        label: '测试',
+        key: 'test',
+        sub: [
+            {
+                path: '/404',
+                label: 'error',
+                key: '404',
+            },
+        ],
+    },
+]
 const Base = function () {
     const handleLogout = () => {
         __storage.set('_token', null)
@@ -75,7 +114,7 @@ const Base = function () {
         return (
             <Menu className={_style.menu} defaultOpenKeys={['sub']}>
                 {menuList.map((it) => {
-                    console.log(it)
+                    // console.log(it)
                     const { path, label, sub, key } = it
                     if (sub && sub.length > 0) {
                         // 有 sub 说明有子选项, 就要处理成带子选项的
@@ -114,29 +153,61 @@ const Base = function () {
             </Menu>
         )
     }
+    const renderHeader = () => {
+        return (
+            <Nav
+                className={_style.nav}
+                direction="hoz"
+                type="primary"
+                header={header}
+                footer={footer}
+                defaultSelectedKeys={['home']}
+                triggerType="hover"
+            >
+                {headerList.map((it) => {
+                    // console.log(it)
+                    const { path, label, sub, key } = it
+                    if (sub && sub.length > 0) {
+                        // 有 sub 说明有子选项, 就要处理成带子选项的
 
+                        // 根据 sub 渲染子菜单
+                        const subList = sub.map((subIt) => {
+                            // 这里解构语法跟上层的解构语法冲突了
+                            // path:subPath 就是类似
+                            // const subPath = subIt.path
+                            const { path: subPath, key: subKey, label: subLabel } = subIt
+                            return (
+                                <Nav.Item key={subKey}>
+                                    {subPath !== undefined ? (
+                                        <Link to={subPath}>{subLabel}</Link>
+                                    ) : (
+                                        subLabel
+                                    )}
+                                </Nav.Item>
+                            )
+                        })
+                        return (
+                            <Nav.SubNav key={key} label={label}>
+                                {subList}
+                            </Nav.SubNav>
+                        )
+                    } else {
+                        // 没有子选项就选染成一级菜单
+                        return (
+                            <Nav.Item key={key}>
+                                {/* 判断一下是否有 path, 有 path 的才需要写成 Link */}
+                                {path !== undefined ? <Link to={path}>{label}</Link> : label}
+                            </Nav.Item>
+                        )
+                    }
+                })}
+            </Nav>
+        )
+    }
     return (
         <div>
-            <div>
-                <Nav
-                    className={_style.nav}
-                    direction="hoz"
-                    type="primary"
-                    header={header}
-                    footer={footer}
-                    defaultSelectedKeys={['home']}
-                    triggerType="hover"
-                >
-                    <Nav.Item key="home">
-                        <Link to={'/'}>主页</Link>
-                    </Nav.Item>
-                    <Nav.SubNav label="Component">
-                        <Nav.Item key="next">Next</Nav.Item>
-                        <Nav.Item key="mext">Mext</Nav.Item>
-                    </Nav.SubNav>
-                    <Nav.Item key="document">Document</Nav.Item>
-                </Nav>
-            </div>
+            <div>{renderHeader()}</div>
+
             <div className={_style.main}>
                 {renderMenu()}
                 <div className={_style.content}>
